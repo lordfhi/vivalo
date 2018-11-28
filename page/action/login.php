5b27e29cce61923c59b0ce8dba7a94ed949ba533
Andika Muhammad Cahya <andika@wannadream.tech>
2018-11-28 18:43:11 +0700

    update view pesanan

--- page/action/login.php
+++ page/action/login.php
@@ -11,15 +11,13 @@ $data = $db->query("select * from vivalo_user where username='$username' and pas
 // menghitung jumlah data yang ditemukan
 if($db->num_rows($data) > 0){
 	$userData = $db->fetch($data);
-	if ($userData->level == "1" || $userData->level == "2")
+	$_SESSION["user"] = $userData;
+	$_SESSION["user_level"] = $userData['level'];
+	if ($userData['level'] == "1" || $userData['level'] == "2")
 	{
-		$_SESSION['username'] = $username;
-		$_SESSION['status'] = "login";
 		header("location: ../admin/index.php");
 	} else
 	{
-		$_SESSION['username'] = $username;
-		$_SESSION['status'] = "login";
 		header("location: ../admin/index.php");
 	}
 } else {
