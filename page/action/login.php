65ef127e2c7e465191e78b211f2d260f596c8430
Andika Muhammad Cahya <andika@wannadream.tech>
2018-11-28 17:31:42 +0700

    update db function

--- page/action/login.php
+++ page/action/login.php
@@ -6,19 +6,13 @@ $username = $_POST['username'];
 $password = md5($_POST['password']);
  
 // menyeleksi data admin dengan username dan password yang sesuai
-$data = $db->db_query("select * from vivalo_user where username='$username' and password='$password'");
+$data = $db->query("select * from vivalo_user where username='$username' and password='$password'");
  
 // menghitung jumlah data yang ditemukan
-$cek = mysqli_num_rows($data);
-
-if($cek > 0){
+if($db->num_rows($data) > 0){
 	$_SESSION['username'] = $username;
 	$_SESSION['status'] = "login";
 	header("location: ../page-customer.php");
 }else{
 header("location: ../page-register.php?pesan=gagal");
 }
-
-
-
-?>
