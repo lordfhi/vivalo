8f081938762c513ae8896cf35c792a9b8479382a
lordfhi <luthfi.ramadan020199@gmail.com>
2018-12-01 01:42:13 +0700

    a

--- page/action/pengeluaranData.php
+++ page/action/pengeluaranData.php
@@ -8,21 +8,15 @@
 			<th>Total Harga</th>
 			<th>Status</th>
 		</tr>
-		<?php 
-		require_once __DIR__."/../../config/config.php";
-		$data = $db->query("SELECT vivalo_laporan_pengeluaran.kd_pembelian, vivalo_pembelian.date, vivalo_pembelian.jumlah_barang, vivalo_pembelian.total_harga, vivalo_laporan_pengeluaran.status FROM `vivalo_laporan_pengeluaran` JOIN vivalo_pembelian ON vivalo_laporan_pengeluaran.kd_pembelian = vivalo_pembelian.kd_pembelian");
-		$no = 1;
-		while($d = $db->fetch($data)){
-		?>
-		<tr>
-			<td><?php echo $no++; ?></td>
-			<td><?php echo $d['vivalo_laporan_pengeluaran.kd_pembelian']; ?></td>
-			<td><?php echo $d['vivalo_pembelian.date']; ?></td>
-			<td><?php echo $d['vivalo_pembelian.jumlah_barang']; ?></td>
-			<td><?php echo $d['vivalo_pembelian.total_harga']; ?></td>
-			<td>x</td>
-		</tr>
-		<?php 
-		}
-		?>
+		   <?php 
+require_once __DIR__."/../../config/config.php";
+foreach($db->query("SELECT * FROM `vivalo_pengeluaran`")->fetch_assoc() as $pengeluaran): ?>
+        <tr>
+            <td><?= $pengeluaran[0] ?></td>
+            <td><?= $pengeluaran[1]?></td>
+            <td><?= $pengeluaran[4] ?> kg</td>
+            <td><?= $pengeluaranp[3]?></td>
+            <td>Rp <?= $pengeluaran[2]?></td>
+        </tr>
+        <?php endforeach ?>
 	</table>
