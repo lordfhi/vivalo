4fcb76577891274702a5fcc4046b489f70fd8df4
lordfhi <luthfi.ramadan020199@gmail.com>
2018-11-30 22:17:40 +0700

    Laporan

--- page/admin/laporan.php
+++ page/admin/laporan.php
@@ -21,50 +21,30 @@ include 'attr_head.php';
             <div class="content-wrapper">
 
               <div class="row">
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+                <div class="col-xl-2 col-lg-3 col-md-2 col-sm-6 grid-margin stretch-card">
                   
                 </div>
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+                <div class="col-xl-4 col-lg-3 col-md-4 col-sm-6 grid-margin stretch-card">
                   <div class="card card-statistics">
                     <div class="card-body">
                       <div class="clearfix">
-                        <div class="float-left">
-                          <i class="mdi mdi-receipt text-warning icon-lg"></i>
-                        </div>
-                        <div class="float-right">
-                          <p class="mb-0 text-right">Orders</p>
-                          <div class="fluid-container">
-                            <h3 class="font-weight-medium text-right mb-0">3455</h3>
-                          </div>
-                        </div>
+                     
+                          <a href="../action/exportPemasukan.php" class="btn btn-primary btn-lg btn-block">Laporan Pemasukan</a>
                       </div>
-                      <p class="text-muted mt-3 mb-0">
-                        <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i> Product-wise sales
-                      </p>
                     </div>
                   </div>
                 </div>
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+                <div class="col-xl-4 col-lg-3 col-md-4 col-sm-6 grid-margin stretch-card">
                   <div class="card card-statistics">
                     <div class="card-body">
                       <div class="clearfix">
-                        <div class="float-left">
-                          <i class="mdi mdi-poll-box text-success icon-lg"></i>
-                        </div>
-                        <div class="float-right">
-                          <p class="mb-0 text-right">Sales</p>
-                          <div class="fluid-container">
-                            <h3 class="font-weight-medium text-right mb-0">5693</h3>
-                          </div>
-                        </div>
+                     
+                          <a href="../action/exportPengeluaran.php" class="btn btn-success btn-lg btn-block">Laporan Pengeluaran</a>
                       </div>
-                      <p class="text-muted mt-3 mb-0">
-                        <i class="mdi mdi-calendar mr-1" aria-hidden="true"></i> Weekly Sales
-                      </p>
                     </div>
                   </div>
                 </div>
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+                <div class="col-xl-2 col-lg-3 col-md-2 col-sm-6 grid-margin stretch-card">
                  
                 </div>
               </div>
