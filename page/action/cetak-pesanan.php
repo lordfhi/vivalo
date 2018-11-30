27689beddb7c39dd06b37d8db46403c96de624e1
Andika Muhammad Cahya <andika@wannadream.tech>
2018-12-01 00:39:09 +0700

    cetak pdf clear

--- page/action/cetak-pesanan.php
+++ page/action/cetak-pesanan.php
@@ -2,3 +2,14 @@
 require_once __DIR__."/../../config/config.php";
 
 // Cetak pesanan
+$kd_pemesanan = $_GET["kd_pemesanan"];
+
+if ($db->query("SELECT * FROM `vivalo_pemesanan` WHERE `kd_pemesanan` = '$kd_pemesanan'")->num_rows > 0)
+{
+	// data pemesanan ada
+	$data_pemesanan = $db->query("SELECT * FROM `vivalo_pemesanan` WHERE `kd_pemesanan` = $kd_pemesanan")->fetch_assoc();
+	$data_konsumen = $db->query("SELECT * FROM `vivalo_konsumen` WHERE `id_konsumen` = '".$data_pemesanan['id_konsumen']."'")->fetch_assoc();
+	$data_paket = $db->query("SELECT * FROM `vivalo_paket` WHERE `kd_paket` = '".$data_pemesanan['kd_paket']."'")->fetch_assoc();
+
+	include_once __DIR__."/../cetak-pesanan.php";
+}
---
+++ page/cetak-pesanan.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<table style="width: 100%">
+	<tr>
+		<td colspan="3" align="center">
+			<h1>STRUK PESAN KONSUMEN
+			<br>
+			VIVALO | Viva Laundry Online</h1>
+		</td>
+	</tr>
+	<tr>
+		<td style="width: 50%; padding-right: 200px;" valign="top" align="right">Nama</td>
+		<td style="width: 2%;">:</td>
+		<td><?= ucfirst($data_konsumen["nama_konsumen"]); ?></td>
+	</tr>
+	<tr>
+		<td style="width: 50%; padding-right: 200px;" valign="top" align="right">Alamat</td>
+		<td style="width: 2%;">:</td>
+		<td><?= $data_konsumen["alamat"] ?></td>
+	</tr>
+	<tr>
+		<td style="width: 50%; padding-right: 200px;" valign="top" align="right">Nomor Telepon</td>
+		<td style="width: 2%;">:</td>
+		<td><?= $data_konsumen["no_hp"] ?></td>
+	</tr>
+	<tr>
+		<td colspan="3" align="center">
+			<h1>*_BERHASIL MELAKUKAN PEMESANAN LAUNDRY_*
+			<br>
+			Pada tanggal <?= $data_pemesanan["date"] ?>,
+			<br>
+			Dengan pesanan paket laundry adalah
+			<br>
+			"<?= strtoupper("PAKET ".$data_paket["nama_paket"]) ?>"</h1>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
